0ffc45626564ab1ecbf82a570230fcc1a9c50af6
Todd Sundsted <todd@sundsted.com>
2012-01-16 18:42:18 -0500

    Added conversions to file input/output streams.

--- src/main/scala/scripting/file/FileBuilder.scala
+++ src/main/scala/scripting/file/FileBuilder.scala
@@ -6,6 +6,10 @@ class FileBuilder(val file: File) {
 
   def f = this
 
+  def << = new java.io.FileOutputStream(this.file)
+
+  def >> = new java.io.FileInputStream(this.file)
+
   override def toString = file.toString
 }
 
